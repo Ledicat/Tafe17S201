cb6b5d8f7b6de04638da78c04d071b12ffe77950
Michelle Britton <michelle.britton@student.tafesa.edu.au>
2017-09-11 16:21:54 +0930

    Adding and deleting of personal details implemented

--- Start-Finance-master/InstaRichie/Views/PersonalDetailsPage.xaml.cs
+++ Start-Finance-master/InstaRichie/Views/PersonalDetailsPage.xaml.cs
@@ -43,8 +43,8 @@ namespace StartFinance.Views
     /// </summary>
     public sealed partial class PersonalDetailsPage : Page
     {
-            SQLiteConnection conn; // adding an SQLite connection
-            string path = Path.Combine(Windows.Storage.ApplicationData.Current.LocalFolder.Path, "Findata.sqlite");
+        SQLiteConnection conn; // adding an SQLite connection
+        string path = Path.Combine(Windows.Storage.ApplicationData.Current.LocalFolder.Path, "Findata.sqlite");
 
         public PersonalDetailsPage()
         {
@@ -66,58 +66,63 @@ namespace StartFinance.Views
         private async void AddPersonalDetails_Click(object sender, RoutedEventArgs e)
         {
             try
+            {
+                //Data Validation
+                if (_FirstName.Text.ToString() == "")
                 {
-                     //Data Validation
-                    if (_FirstName.Text.ToString() == "")
-                    {
-                        MessageDialog dialog = new MessageDialog("No first name entered", "Oops..!");
-                        await dialog.ShowAsync();
-                    }
-                    else if (_LastName.Text.ToString() == "")
-                    {
-                        MessageDialog dialog = new MessageDialog("No last name entered", "Oops..!");
-                        await dialog.ShowAsync();
-                    }
-                    else if (_DateOfBirth.ToString() == "")
-                    {
-                        MessageDialog dialog = new MessageDialog("No date of birth entered", "Oops..!");
-                        await dialog.ShowAsync();
-                    }
-                    else if (_Gender.SelectedValue.ToString() == "")
-                    {
-                        MessageDialog dialog = new MessageDialog("No gender entered", "Oops..!");
-                        await dialog.ShowAsync();
-                    }
-                    else if (_EmailAddress.Text.ToString() == "")
-                    {
-                        MessageDialog dialog = new MessageDialog("No email entered", "Oops..!");
-                        await dialog.ShowAsync();
-                    }
+                    MessageDialog dialog = new MessageDialog("No first name entered", "Oops..!");
+                    await dialog.ShowAsync();
+                }
+                else if (_LastName.Text.ToString() == "")
+                {
+                    MessageDialog dialog = new MessageDialog("No last name entered", "Oops..!");
+                    await dialog.ShowAsync();
+                }
+                else if (_DateOfBirth.ToString() == "")
+                {
+                    MessageDialog dialog = new MessageDialog("No date of birth entered", "Oops..!");
+                    await dialog.ShowAsync();
+                }
+                else if (_Gender.SelectedIndex == -1)
+                {
+                    MessageDialog dialog = new MessageDialog("No gender entered", "Oops..!");
+                    await dialog.ShowAsync();
+                }
+                else if (_EmailAddress.Text.ToString() == "")
+                {
+                    MessageDialog dialog = new MessageDialog("No email entered", "Oops..!");
+                    await dialog.ShowAsync();
+                }
+                else if (_PhoneNo.Text.ToString() == "")
+                {
+                    MessageDialog dialog = new MessageDialog("No phone number entered", "Oops..!");
+                    await dialog.ShowAsync();
+                }
 
-
+                //Validation of Email and Phone number is not required, KT says.
 
                 else
                 {
-                   
+                    
                     conn.CreateTable<PersonalDetails>();
                     conn.Insert(new PersonalDetails
                     {
                         FirstName = _FirstName.Text.ToString(),
+                        LastName = _LastName.Text.ToString(),
+                        DateOfBirth = _DateOfBirth.Date.ToString("d"),
+                        Gender = _Gender.SelectedValue.ToString(),
+                        Email = _EmailAddress.Text.ToString(),
+                        Phone = _PhoneNo.Text.ToString()
                     });
                     // Creating table
                     Results();
                 }
             }
-                catch (Exception ex)
+            catch (Exception ex)
+            {
+                if (ex is FormatException)
                 {
-                    if (ex is FormatException)
-                   {
-                        MessageDialog dialog = new MessageDialog("You forgot to enter the Amount or entered an invalid Amount", "Oops..!");
-                        await dialog.ShowAsync();
-                    }
-                else if (ex is SQLiteException)
-                {
-                    MessageDialog dialog = new MessageDialog("Wish Name already exist, Try Different Name", "Oops..!");
+                    MessageDialog dialog = new MessageDialog("Format Exception has Occured! ", "Oops..!");
                     await dialog.ShowAsync();
                 }
                 else
@@ -127,35 +132,39 @@ namespace StartFinance.Views
             }
         }
 
-            private async void DeleteItem_Click(object sender, RoutedEventArgs e)
-            {
+        private async void DeleteItem_Click(object sender, RoutedEventArgs e)
+        {
             try
             {
-                string AccSelection = ((WishList)PersonalDetailsView.SelectedItem).WishName;
+                string AccSelection = ((PersonalDetails)PersonalDetailsView.SelectedItem).FirstName;
                 if (AccSelection == "")
                 {
-                    MessageDialog dialog = new MessageDialog("Not selected the Item", "Oops..!");
+                    MessageDialog dialog = new MessageDialog("No Item is selected", "Oops..!");
                     await dialog.ShowAsync();
                 }
                 else
                 {
-                    conn.CreateTable<WishList>();
-                    var query1 = conn.Table<WishList>();
-                    var query3 = conn.Query<WishList>("DELETE FROM WishList WHERE WishName ='" + AccSelection + "'");
+                    conn.CreateTable<PersonalDetails>();
+                    var query1 = conn.Table<PersonalDetails>();
+                    var query3 = conn.Query<PersonalDetails>("DELETE FROM PersonalDetails WHERE FirstName ='" + AccSelection + "'");
                     PersonalDetailsView.ItemsSource = query1.ToList();
                 }
             }
             catch (NullReferenceException)
-                {
-                    MessageDialog dialog = new MessageDialog("Not selected the Item", "Oops..!");
-                    await dialog.ShowAsync();
-                }
+            {
+                MessageDialog dialog = new MessageDialog("No Item is selected", "Oops..!");
+                await dialog.ShowAsync();
             }
+        }
 
         private void Page_Loaded(object sender, RoutedEventArgs e)
         {
             Results();
         }
 
+        private void EditPersonalDetails_Click(object sender, RoutedEventArgs e)
+        {
+           
+        }
     }
 }
